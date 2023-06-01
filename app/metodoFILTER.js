7c4308a998cb4754ac48958a4da30a53c45b256f
Cassio Jhones <56178855+CassioJhones@users.noreply.github.com>
2023-06-01 04:56:37 -0300

    Add files via upload

---
+++ app/metodoFILTER.js
@@ -0,0 +1,32 @@
+const botoes = document.querySelectorAll(".btn");
+botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
+
+function filtrarLivros() {
+  const elementoBtn = document.getElementById(this.id);
+  const categoria = elementoBtn.value;
+  let livrosFiltrados =
+    categoria == "disponivel"
+      ? fitroDisponibilidade()
+      : filtroCategoria(categoria);
+  exibirLivrosNaTela(livrosFiltrados);
+  if (categoria == "disponivel") {
+    const valorTotal = ValorTotalDISPONIVEIS(livrosFiltrados)
+    exibirValorTotal(valorTotal);
+  }
+}
+
+function filtroCategoria(categoria) {
+  return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function fitroDisponibilidade() {
+  return livros.filter((livro) => livro.quantidade > 0);
+}
+
+function exibirValorTotal(valorTotal) {
+  valorTotalLivrosDisponiveis.innerHTML = `
+  <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `;
+}
---
+++ app/metodoFOREACH.js
@@ -0,0 +1,35 @@
+const elementoInsereLivros = document.getElementById("livros");
+const valorTotalLivrosDisponiveis = document.getElementById(
+  "valor_total_livros_disponiveis"
+);
+
+function exibirLivrosNaTela(listaDeLivros) {
+  valorTotalLivrosDisponiveis.innerHTML = "";
+  elementoInsereLivros.innerHTML = "";
+  listaDeLivros.forEach((livro) => {
+    let disponibilidade = verificaEstoque(livro);
+    elementoInsereLivros.innerHTML += `
+    
+    <div class="livro">
+      <img class="${disponibilidade}" src="${livro.imagem}"
+        alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+      ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>
+    `;
+  });
+}
+// verifica se a quantidade de livros é maior que 0 e adiciona a classe o seletor CSS correto
+function verificaEstoque(livro) {
+  if (livro.quantidade > 0) {
+    return "livro__imagens";
+  } else {
+    return "livro__imagens indisponivel";
+  }
+}
