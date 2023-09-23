8a9837c4f7db82a02ad21d6584c06163be0586e7
Cassio Jhones <56178855+CassioJhones@users.noreply.github.com>
2023-09-23 01:57:27 -0300

    Update metodoMAP.js

--- app/metodoMAP.js
+++ app/metodoMAP.js
@@ -1,8 +1,6 @@
 function aplicarDesconto(livros) {
   const desconto = 0.3;
   livroscomDesconto = livros.map((livro) => {
-    return { ...livro, preco: livro.preco - livro.preco * desconto };
-  });
+    return { ...livro, preco: livro.preco - livro.preco * desconto };  });
 
-  return livroscomDesconto;
-}
+  return livroscomDesconto;}
